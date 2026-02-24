ee13d590170d3b9059441ef55076c9b5307da096
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-02-24 22:04:37 +0530

    output 3

--- insert into statement.sql
+++ insert into statement.sql
@@ -132,3 +132,80 @@ xxxxxxxxxx              27
 
 
 6 rows selected.
+
+OUTPUT 3 : Inserting Data into Specific Columns
+
+SQL> insert into stu_mrtn(roll_no,name,age)
+  2   values (7,'nehru',27);
+
+1 row created.
+
+SQL> desc stu_mrtn;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ROLL_NO                                   NOT NULL NUMBER(38)
+ NAME                                               VARCHAR2(50)
+ ADDRESS                                            VARCHAR2(100)
+ PHONE                                              VARCHAR2(15)
+ AGE                                                NUMBER(38)
+
+SQL> select * from stu_mrtn;
+
+   ROLL_NO NAME
+---------- --------------------------------------------------
+ADDRESS
+--------------------------------------------------------------------------------
+PHONE                  AGE
+--------------- ----------
+         1 Sophia
+Berlin
+xxxxxxxxxx              18
+
+         2 alexa
+tokyo
+xxxxxxxxxx              19
+
+   ROLL_NO NAME
+---------- --------------------------------------------------
+ADDRESS
+--------------------------------------------------------------------------------
+PHONE                  AGE
+--------------- ----------
+
+         3 santa
+berlin
+xxxxxxxxxx              20
+
+         4 akira
+india
+
+   ROLL_NO NAME
+---------- --------------------------------------------------
+ADDRESS
+--------------------------------------------------------------------------------
+PHONE                  AGE
+--------------- ----------
+xxxxxxxxxx              21
+
+         5 anatha
+india
+xxxxxxxxxx              17
+
+         6 chitti
+
+   ROLL_NO NAME
+---------- --------------------------------------------------
+ADDRESS
+--------------------------------------------------------------------------------
+PHONE                  AGE
+--------------- ----------
+usa
+xxxxxxxxxx              27
+
+         7 nehru
+
+                        27
+
+
+7 rows selected.
+
