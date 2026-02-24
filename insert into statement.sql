e219905e765c974e72ce6e9d0b8bb820bb4cacec
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-02-24 21:58:52 +0530

    output 2

--- insert into statement.sql
+++ insert into statement.sql
@@ -71,4 +71,64 @@ india
 xxxxxxxxxx              17
 
 
-SQL>
+OUTPUT 2 : INSERT A ADD DATA
+
+SQL> insert into stu_mrtn values (6, 'chitti', 'usa', 'xxxxxxxxxx', 27);
+
+1 row created.
+
+SQL> select * from stu_mrtn;
+
+   ROLL_NO NAME
+---------- --------------------------------------------------
+ADDRESS
+--------------------------------------------------------------------------------
+PHONE                  AGE
+--------------- ----------
+         1 Sophia
+Berlin
+xxxxxxxxxx              18
+
+         2 alexa
+tokyo
+xxxxxxxxxx              19
+
+   ROLL_NO NAME
+---------- --------------------------------------------------
+ADDRESS
+--------------------------------------------------------------------------------
+PHONE                  AGE
+--------------- ----------
+
+         3 santa
+berlin
+xxxxxxxxxx              20
+
+         4 akira
+india
+
+   ROLL_NO NAME
+---------- --------------------------------------------------
+ADDRESS
+--------------------------------------------------------------------------------
+PHONE                  AGE
+--------------- ----------
+xxxxxxxxxx              21
+
+         5 anatha
+india
+xxxxxxxxxx              17
+
+         6 chitti
+
+   ROLL_NO NAME
+---------- --------------------------------------------------
+ADDRESS
+--------------------------------------------------------------------------------
+PHONE                  AGE
+--------------- ----------
+usa
+xxxxxxxxxx              27
+
+
+6 rows selected.
