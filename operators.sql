1443b955b20e817b09f23f0788b45f1ca15580bb
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-03-02 22:25:39 +0530

    INTRO

---
+++ operators.sql
@@ -0,0 +1,3 @@
+SQL - Logical Operators
+
+SQL Logical Operators are used to test conditions in queries, returning results as TRUE, FALSE, or UNKNOWN. They help in combining, negating, and comparing conditions, enabling precise data retrieval and filtering.
