a537ca7e96d52d121165f4c6df6c011ef10554e8
PulluriHarshitha <pulluriharshitha90@gmail.com>
2026-03-11 21:10:45 +0530

    OUTPUT 3

--- NOT EQUAL operator.sql
+++ NOT EQUAL operator.sql
@@ -73,3 +73,24 @@ OUTPUT 1 : SQL> select * from hostel
          5 reethu             27 cec                                           wgl
          6 chinni             24 deg                                           jgl
 
+OUTPUT 2 : SQL> select * from hostel
+  2  where name <> 'vaishu';
+
+        ID NAME              AGE COLLEGE                                       CITY
+---------- ---------- ---------- -------------------------------------------------- ---------------
+         1 harshu             20 smec                                          knr
+         3 teju               22 cmrit                                         vmd
+         4 shiny              26 ku                                            mncl
+         5 reethu             27 cec                                           wgl
+         6 chinni             24 deg                                           jgl
+
+OUTPUT 3 :SQL> select * from hostel
+  2  where city != 'vmd';
+
+        ID NAME              AGE COLLEGE                                       CITY
+---------- ---------- ---------- -------------------------------------------------- ---------------
+         1 harshu             20 smec                                          knr
+         2 vaishu             20 smec                                          hsbd
+         4 shiny              26 ku                                            mncl
+         5 reethu             27 cec                                           wgl
+         6 chinni             24 deg                                           jgl
